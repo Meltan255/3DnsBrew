c1f4b416824f321408b2cf1e334ec0f8cc6afa79
Meltan255 <134165155+Meltan255@users.noreply.github.com>
2023-06-14 06:30:21 -0700

    Launches luma put in root

--- 3DnsbrewLauncher.cpp
+++ 3DnsbrewLauncher.cpp
@@ -6,26 +6,35 @@
 
 using namespace std;
 
-int main() {
-    string dns = "18.188.135.9";
+bool isConnectedToInternet(const string& dns) {
     string command = "ping -n 1 " + dns;
     int result = system(command.c_str());
-    if (result == 0) {
-        cout << "Connected to the internet with DNS " << dns << endl;
-        if (access("/luma/", F_OK) != -1) {
-            cout << "Luma is already installed" << endl;
-            string reboot = "reboot";
-            system(reboot.c_str());
-        } else {
-            string lumaInstall = "https://github.com/LumaTeam/Luma3DS/releases/download/v12.0.1/Luma3DSv12.0.1.zip";
-            string lumaCommand = "mkdir /luma/ && curl -L " + lumaInstall + " -o Luma3DSv12.0.1.zip && unzip Luma3DSv12.0.1.zip && cp -r Luma3DS /luma/ && rm -rf Luma3DSv12.0.1.zip";
-            system(lumaCommand.c_str());
-            cout << "Luma3DS has been installed" << endl;
-            string reboot = "reboot";
-            system(reboot.c_str());
-        }
-    } else {
+    return result == 0;
+}
+
+int main() {
+    string dns = "18.188.135.9";
+
+    // Check for internet connection and specific DNS
+    while (!isConnectedToInternet(dns)) {
         cout << "Not connected to the internet with DNS " << dns << endl;
+        sleep(5); // Wait for 5 seconds before checking again
     }
+
+    cout << "Connected to the internet with DNS " << dns << endl;
+
+    if (access("/luma/", F_OK) != -1) {
+        cout << "Luma is already installed" << endl;
+        string reboot = "reboot";
+        system(reboot.c_str());
+    } else {
+        string lumaInstall = "https://github.com/LumaTeam/Luma3DS/releases/download/v12.0.1/Luma3DSv12.0.1.zip";
+        string lumaCommand = "mkdir /luma/ && curl -L " + lumaInstall + " -o Luma3DSv12.0.1.zip && unzip Luma3DSv12.0.1.zip && cp -r Luma3DS /luma/ && rm -rf Luma3DSv12.0.1.zip";
+        system(lumaCommand.c_str());
+        cout << "Luma3DS has been installed" << endl;
+        string reboot = "reboot";
+        system(reboot.c_str());
+    }
+
     return 0;
 }
